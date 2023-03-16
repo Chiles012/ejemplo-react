2c2b57e9d776016f25beac2b3314c0abd1fb8855
Christian Axel Serrano Sandoval ( Chiles ) <61697287+Chiles012@users.noreply.github.com>
2023-03-15 21:50:12 -0600

    uclass 15-mar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,84 @@
-import logo from './logo.svg';
+// Librerias
+import { useState } from 'react';
+
+// Componentes
+import Button from './Button';
+
+// Estilos
 import './App.css';
 
-import Component1 from './Componente1';
-import Componente2 from './Componente2';
-
 function App() {
+
+  /**
+   * Estado
+   * const [ valor, setValor ] = useState(valorInicial)
+   */
+  const [number, setNumber] = useState(0);
+  let component = null
+  let componentCase = null
+
+  if (number%2 === 0) {
+    component = <h1>Es par</h1>
+  } else {
+    component = <h1>Es impar</h1>
+  }
+
+  switch(number) {
+    case 10:
+      componentCase = <h1>Numero 100</h1>
+      break
+    case 20:
+      componentCase = <h1>Numero 100</h1>
+      break;
+      default:
+        componentCase = null
+        break;
+  }
+
+  let array = [
+    {
+      id: 1,
+      nombre: "Ricardo"
+    },
+    {
+      id: 2,
+      nombre: "Alexis"
+    },
+    {
+      id: 3,
+      nombre: "Luis"
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Component1 />
-        <Componente2 />
-      </header>
-    </div>
+    <> { /* Fragment */ }
+      <h1>
+        {number}
+      </h1>
+      {component}
+      {componentCase}
+      {
+        array.map((value, index) => (
+          <div
+            key={index}
+          >
+            {value.nombre}
+          </div>
+        ))
+      }
+      <Button
+        text="Incrementar"
+        event={() => setNumber(number + 1)}
+      />
+      <Button
+        text="Decrementar"
+        event={() => setNumber(number - 1)}
+      />
+      <Button
+        text="Resetear"
+        event={() => setNumber(0)}
+      />
+    </>
   );
 }
 
